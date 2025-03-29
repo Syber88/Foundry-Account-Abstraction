b92a5d8c9c9c801630130c36290c02f041417a36
Syber88 <siyamgz88@gmail.com>
2025-03-29 20:00:20 +0200

    finished get config functoin and partially done with deploy scriopt

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,6 +13,7 @@ contract HelperConfig is Script {
 
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC = 300;
+    uint256 constant LOCAL_CHAIN_ID = 31337;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -21,8 +22,33 @@ contract HelperConfig is Script {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
     } 
 
-function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
-    return NetworkConfig({entryPoint: })
-}
+    function getConfig() public returns(NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory) {
+        if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilEthConfig();
+
+        }else if (networkConfigs[chainId].entryPoint != address(0)){
+            return networkConfigs[chainId];
+        }else {
+            revert HelperConfig__InvalidCahinId();
+        }
+    }
+
+    function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789});
+    }
+
+    function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({entryPoint: address(0)});
+    }
+
+    function getOrCreateAnvilEthConfig() public view returns(NetworkConfig memory) {
+        if (localNetworkConfig.entryPoint != address(0)) {
+            return localNetworkConfig;
+        }
+    }
 
 }
