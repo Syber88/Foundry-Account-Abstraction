93da7265e79b9d22f7fec3e369f63787a7ff113d
Syber88 <siyamgz88@gmail.com>
2025-03-30 17:41:40 +0200

    fixed build issues

--- script/SendPackedUser.s.sol
+++ script/SendPackedUser.s.sol
@@ -3,19 +3,27 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract SendPackerUserOp is Script {
-    function run() public {
+    function run() public {}
 
+    function generateSignedUserOperation(bytes memory callData, HelperConfig.NetworkConfig memory config)
+        public
+        returns (PackedUserOperation memory)
+    {
+        uint256 nonce = vm.getNonce(config.account);
+        PackedUserOperation memory unsignerUserOp = _generateUnsignedUserOperation(callData, config.account, nonce);
+
+        bytes32 userOpHash = config.entryPoint;
     }
 
-    function generateSignedUserOperation(bytes memory callData, address sender) public returns (PackedUserOperation memory) {
-        uint256 nonce = vm.getNonce(sender);
-        PackedUserOperation memory unsignerUserOp = _generateUnsignedUserOperation(callData, sender, nonce);
-
-    }
-
-    function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce) internal pure returns (PackedUserOperation memory){
+    function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
+        internal
+        pure
+        returns (PackedUserOperation memory)
+    {
         uint128 verificationGasLimit = 16777216;
         uint128 callGasLimit = verificationGasLimit;
         uint128 maxPriorityFeePerGas = 256;
@@ -23,15 +31,14 @@ contract SendPackerUserOp is Script {
 
         return PackedUserOperation({
             sender: sender,
-            nonce: nonce, 
+            nonce: nonce,
             initCode: hex"",
             callData: callData,
             accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
-            preVerificationGas: verificationGasLimit, 
-            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas), 
+            preVerificationGas: verificationGasLimit,
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
             paymasterAndData: hex"",
             signature: hex""
         });
     }
-
 }
