e65f88df88a8477aaddb2a1dab3667ee1f56f97c
Syber88 <siyamgz88@gmail.com>
2025-03-31 14:48:31 +0200

    test passes

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -17,7 +17,8 @@ contract HelperConfig is Script {
     uint256 constant ZKSYNC = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
     address constant BURNER_WALLET = 0xaCce6B90C08Daa3fA9fbEa88c27c248bB4308c47;
-    address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+    // address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+    address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -54,10 +55,12 @@ contract HelperConfig is Script {
         }
 
         console2.log("Deploying mocks...");
-        vm.startBroadcast();
+        vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
         EntryPoint entryPoint = new EntryPoint();
         vm.stopBroadcast();
 
-        return NetworkConfig({entryPoint: address(entryPoint), account: FOUNDRY_DEFAULT_WALLET});
+        localNetworkConfig = NetworkConfig({entryPoint: address(entryPoint), account: ANVIL_DEFAULT_ACCOUNT});
+
+        return localNetworkConfig;
     }
 }
