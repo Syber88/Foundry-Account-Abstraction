35b8cb0e13bf6b5cf8d47612d1eb5091c88831a5
Syber88 <siyamgz88@gmail.com>
2025-04-01 19:15:53 +0200

    partially done with testing wheteher zk owner can execute comands or not

--- test/zkSync/ZkMinimalAccountTest.t.sol
+++ test/zkSync/ZkMinimalAccountTest.t.sol
@@ -37,4 +37,32 @@ contract ZkMinimalAccountTest is Test, ZkSyncChainChecker {
         usdc = new ERC20Mock();
         vm.deal(address(minimalAccount), AMOUNT);
     }
+
+    function testZkOwnerCanExecuteCommands() public {
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functIOnData = abi.encodeWithSelector(ERC20Mock.min.selector, address(minimalAccount), ACCOUNT);
+
+        Transation memory Transaction = _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functIOnData)
+    }
+
+    function _createUnsignedTransaction(address from, uint8 transactionType, address to, uint256 value, bytes memory data) internal view returns(Transaction memory) {
+        uint256 nonce = vm.getNonce(address(minimalAccount));
+        bytes32[] memory factoryDeps = new bytes32[](0);
+        return Transaction({
+            txType: transactionType, 
+            from: uint256(uint160(from)), 
+            to: uint256(uint160(to)),
+            gasLimit: 16777216,
+            gasPerPubdataByteLimit: 16777216,
+            maxFeePerGas: 16777216,
+            paymaster: 0, 
+            nonce: nonce,
+            value: value, 
+            reserved: [uint256(0),uint256(0),uint256(0),uint256(0)], 
+            data: data, 
+            signature: hex"", 
+            factoryDeps: factoryDeps
+        });
+    }
 }
