329fca1f3dc4e2d4be562089d70fff8eb570a0e2
Syber88 <siyamgz88@gmail.com>
2025-04-01 22:01:15 +0200

    added helper function to sign transactoins

--- test/zkSync/ZkMinimalAccountTest.t.sol
+++ test/zkSync/ZkMinimalAccountTest.t.sol
@@ -51,6 +51,15 @@ contract ZkMinimalAccountTest is Test, ZkSyncChainChecker {
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
     }
 
+    function testZkValidateTransaction() public {
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        Transaction memory transaction = _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+
+    }
+
     function _createUnsignedTransaction(address from, uint8 transactionType, address to, uint256 value, bytes memory data) internal view returns(Transaction memory) {
         uint256 nonce = vm.getNonce(address(minimalAccount));
         bytes32[] memory factoryDeps = new bytes32[](0);
@@ -73,4 +82,18 @@ contract ZkMinimalAccountTest is Test, ZkSyncChainChecker {
             reservedDynamic: hex""
         });
     }
+
+    function _signTransaction(Transaction memory transaction) internal view returns (Transaction memory) {
+        bytes32 unsignedTransactionHash = MemoryTransactionHelper.encodeHash(transaction);
+        // bytes32 digest = unsignedTransactionHash.toEthSignedMessageHash();
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, unsignedTransactionHash);
+        Transaction memory signedTransaction = transaction;
+        signedTransaction.signature = abi.encodePacked(r, s, v);
+        return signedTransaction;
+    }
+
 }
