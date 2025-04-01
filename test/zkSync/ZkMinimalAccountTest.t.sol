e86e564582159fe2be02533cb92a1a341d0d378d
Syber88 <siyamgz88@gmail.com>
2025-04-01 22:07:10 +0200

    completed test for validating transaction

--- test/zkSync/ZkMinimalAccountTest.t.sol
+++ test/zkSync/ZkMinimalAccountTest.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import {Test, console2} from "forge-std/Test.sol";
@@ -43,7 +43,8 @@ contract ZkMinimalAccountTest is Test, ZkSyncChainChecker {
         uint256 value = 0;
         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
 
-        Transaction memory transaction = _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+        Transaction memory transaction =
+            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
 
         vm.prank(minimalAccount.owner());
         minimalAccount.executeTransaction(EMPTY_BYTES32, EMPTY_BYTES32, transaction);
@@ -51,32 +52,46 @@ contract ZkMinimalAccountTest is Test, ZkSyncChainChecker {
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
     }
 
-    function testZkValidateTransaction() public {
+    function testZkValidateTransaction() public onlyZkSync {
+        // Arrange
         address dest = address(usdc);
         uint256 value = 0;
         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+        Transaction memory transaction =
+            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+        transaction = _signTransaction(transaction);
 
-        Transaction memory transaction = _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+        // Act
+        vm.prank(BOOTLOADER_FORMAL_ADDRESS);
+        bytes4 magic = minimalAccount.validateTransaction(EMPTY_BYTES32, EMPTY_BYTES32, transaction);
 
+        // Assert
+        assertEq(magic, ACCOUNT_VALIDATION_SUCCESS_MAGIC);
     }
 
-    function _createUnsignedTransaction(address from, uint8 transactionType, address to, uint256 value, bytes memory data) internal view returns(Transaction memory) {
+    function _createUnsignedTransaction(
+        address from,
+        uint8 transactionType,
+        address to,
+        uint256 value,
+        bytes memory data
+    ) internal view returns (Transaction memory) {
         uint256 nonce = vm.getNonce(address(minimalAccount));
         bytes32[] memory factoryDeps = new bytes32[](0);
         return Transaction({
-            txType: transactionType, 
-            from: uint256(uint160(from)), 
+            txType: transactionType,
+            from: uint256(uint160(from)),
             to: uint256(uint160(to)),
             gasLimit: 16777216,
             gasPerPubdataByteLimit: 16777216,
             maxPriorityFeePerGas: 16777216,
             maxFeePerGas: 16777216,
-            paymaster: 0, 
+            paymaster: 0,
             nonce: nonce,
-            value: value, 
-            reserved: [uint256(0),uint256(0),uint256(0),uint256(0)], 
-            data: data, 
-            signature: hex"", 
+            value: value,
+            reserved: [uint256(0), uint256(0), uint256(0), uint256(0)],
+            data: data,
+            signature: hex"",
             factoryDeps: factoryDeps,
             paymasterInput: hex"",
             reservedDynamic: hex""
@@ -95,5 +110,4 @@ contract ZkMinimalAccountTest is Test, ZkSyncChainChecker {
         signedTransaction.signature = abi.encodePacked(r, s, v);
         return signedTransaction;
     }
-
 }
